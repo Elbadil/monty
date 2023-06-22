740f2fba527ce1917104dc3419daf0e355824bb8
Elbadil <elbaliadil@gmail.com>
2023-06-22 14:22:45 -0700

    Nop command that does nothing

---
+++ op_nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * op_nop - Does nothing
+ * @stack: Double pointer to the top of a stack
+ * @line_number: keeps track of lines in the file
+*/
+
+void op_nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
