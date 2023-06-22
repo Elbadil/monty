de90378819c885b92da34715aadb4113231eb8c1
Elbadil <elbaliadil@gmail.com>
2023-06-22 14:28:13 -0700

    Subs the top two elements

---
+++ op_sub.c
@@ -0,0 +1,41 @@
+#include "monty.h"
+
+/**
+ * op_sub - Subs the top element from the second element in the stack
+ * @stack: Double pointer to the top of a stack
+ * @line_number: keeps track of lines in the file
+*/
+
+void op_sub(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+	int first, second;
+	int result = 0;
+	stack_t *current = *stack;
+	int counter = 0;
+
+	while (current)
+	{
+		counter++;
+		current = current->next;
+	}
+
+	if (counter < 2)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		fclose(op.op_file);
+		free(op.op_cmd);
+		free_stack_list(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	first = (*stack)->n;
+	second = (*stack)->next->n;
+	result = second - first;
+
+	(*stack)->next->n = result;
+
+	temp = (*stack)->next;
+	free(*stack);
+	*stack = temp;
+}
