8771117086661f3cef6b8fcc3ff9d72ea9cb3f62
Elbadil <elbaliadil@gmail.com>
2023-06-22 14:16:56 -0700

    Header File

---
+++ monty.h
@@ -0,0 +1,74 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#define _POSIX_C_SOURCE 200809L
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/**
+ * struct op_s - struct so store op file, cmd and arguments
+ * @op_file: file contains op codes
+ * @op_cmd: opcode command
+ * @op_arg: opcode arguments
+*/
+typedef struct op_s
+{
+	FILE *op_file;
+	char *op_cmd;
+	char *op_arg;
+}  op_t;
+extern op_t op;
+
+/* opcode functions */
+void op_pop(stack_t **stack, unsigned int line_number);
+void op_swap(stack_t **stack, unsigned int line_number);
+void op_add(stack_t **stack, unsigned int line_number);
+void op_nop(stack_t **stack, unsigned int line_number);
+void op_pall(stack_t **stack, unsigned int line_number);
+void op_pint(stack_t **stack, unsigned int line_number);
+void op_push(stack_t **stack, unsigned int line_number);
+void op_sub(stack_t **stack, unsigned int line_number);
+void op_div(stack_t **stack, unsigned int line_number);
+void op_mul(stack_t **stack, unsigned int line_number);
+void op_mod(stack_t **stack, unsigned int line_number);
+void op_pchar(stack_t **stack, unsigned int line_number);
+
+/* other functions */
+int main(int argc, char **argv);
+void free_stack_list(stack_t *stack);
+int op_execute(stack_t **stack, char *cmds, unsigned int line, FILE *op_file);
+stack_t *add_node_beg(stack_t **head, const int n);
+
+#endif
